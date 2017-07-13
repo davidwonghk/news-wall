70c9c86babbc8e374545b6b5704220d9b245013f
dbc news <ddbc.news@gmail.com>
2017-07-13 22:36:35 +0100

    remove local image file when remove image form mongo

--- models/Image.js
+++ models/Image.js
@@ -3,6 +3,7 @@ var Types = keystone.Field.Types;
 var cloudinary = require('cloudinary');
 
 var util = require('../crawl/util')
+var fs = require('fs');
 
 /**
  * Image Model
@@ -15,17 +16,24 @@ var Image = new keystone.List('Image');
 Image.add({
 	cloudinary: { type: Types.CloudinaryImage },
 	reference: { type: Types.Url },
-	local: {type: Types.Text},		//the local url after downloading
 });
 
+
+Image.schema.virtual('localPath').get(function() {
+	const IMAGE_LOCAL_DIR = 'public/img/'
+	return IMAGE_LOCAL_DIR + this.id;
+});
+
+Image.schema.virtual('local').get(function() {
+	'/img/' + this.id;
+});
 
 
 Image.schema.methods.publish = function(post, callback) {
 		if (!this.reference) return callback('reference not found');
 
 		var headers = {Referer: post.reference};
-		this.local = "/img/" + this.id;
-		util.download(this.reference, 'public'+this.local, headers, function() {
+		util.download(this.reference, this.localPath, headers, function() {
 			cloudinary.uploader.upload(this.local, function(result) {
 				this.cloudinary = result;
 				this.save(callback);
@@ -33,6 +41,11 @@ Image.schema.methods.publish = function(post, callback) {
 		}.bind(this));
 
 }
+
+
+Image.schema.pre('remove', function(next) {
+	fs.unlink(this.localPath, next)
+});
 
 
 Image .defaultColumns = 'reference, cloudinary|30%'
